57f93cc2a4408851ed61b7a120a06ea29c6d9594
Daniel Pierce <dlpierce@indiana.edu>
2019-07-11 15:44:53 -0400

    Refactor to resolve code drift and use original_file_id

--- spec/presenters/hyrax/file_set_presenter_spec.rb
+++ spec/presenters/hyrax/file_set_presenter_spec.rb
@@ -86,7 +86,7 @@ RSpec.describe Hyrax::FileSetPresenter do
        "subject", "language", "license", "format_label", "file_size",
        "height", "width", "filename", "well_formed", "page_count",
        "file_title", "last_modified", "original_checksum", "mime_type",
-       "duration", "sample_rate", "current_file_version"]
+       "duration", "sample_rate", "original_file_id"]
     end
 
     it "delegates to the solr_document" do
